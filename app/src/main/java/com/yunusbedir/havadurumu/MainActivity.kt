a7d53fce7e9ff97df062bc5ed584537da4f039df
Yunus Bedir <yunusbedir@outlook.com.tr>
2020-05-13 02:39:15 +0300

    tr_kordinatları eklendi. ReadCsv oluşturularak csv deki satırlar okundu. Region Data Class ı oluşuturuldu

--- app/src/main/java/com/yunusbedir/havadurumu/MainActivity.kt
+++ app/src/main/java/com/yunusbedir/havadurumu/MainActivity.kt
@@ -2,11 +2,14 @@ package com.yunusbedir.havadurumu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yunusbedir.havadurumu.Repository.Csv.ReadCsv
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ReadCsv(this).getSearchedList("Sul")
     }
 }
---
+++ app/src/main/java/com/yunusbedir/havadurumu/Model/Region.kt
@@ -0,0 +1,12 @@
+package com.yunusbedir.havadurumu.Model
+
+
+/**
+ * Created by YUNUS BEDİR on 13.05.2020.
+ */
+data class Region(
+    val name: String?,
+    val lat: Int?,
+    val lon: Int?
+) {
+}
