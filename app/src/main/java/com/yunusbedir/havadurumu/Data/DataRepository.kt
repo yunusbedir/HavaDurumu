f5975b44342a04d055c831ac1ae9877824e3274e
Yunus Bedir <yunusbedir@outlook.com.tr>
2020-05-14 03:52:20 +0300

    CurrentViewModel oluşturuldu

--- app/src/main/java/com/yunusbedir/havadurumu/Data/DataRepository.kt
+++ app/src/main/java/com/yunusbedir/havadurumu/Data/DataRepository.kt
@@ -1,12 +1,9 @@
 package com.yunusbedir.havadurumu.Data
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.util.Log
 import com.yunusbedir.havadurumu.Data.Api.WeatherService
-import com.yunusbedir.havadurumu.Data.Csv.ReadCsv
 import com.yunusbedir.havadurumu.Model.BaseWeather
-import com.yunusbedir.havadurumu.Model.Region
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
@@ -15,30 +12,27 @@ import io.reactivex.schedulers.Schedulers
 /**
  * Created by YUNUS BEDİR on 13.05.2020.
  */
-class DataRepository(private val context: Context) {
+class DataRepository {
 
     @SuppressLint("CheckResult")
     fun getWeather(
         lat: Int,
         lon: Int,
-        operationCallBack: OperationCallBack<BaseWeather>
+        onSuccess: (BaseWeather) -> Unit,
+        onError: (Throwable) -> Unit
     ) {
         WeatherService.getWeather(lat, lon)
             .subscribeOn(Schedulers.newThread())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeWith(object : DisposableSingleObserver<BaseWeather>() {
                 override fun onSuccess(baseWeather: BaseWeather) {
-                    operationCallBack.onSuccess(baseWeather)
+                    onSuccess(baseWeather)
                 }
 
                 override fun onError(e: Throwable) {
-                    operationCallBack.onError(e.localizedMessage!!)
+                    onError(e)
                 }
 
             })
-    }
-
-    fun getSearchedList(text: String): List<Region> {
-        return ReadCsv(context).getSearchedList(text)
     }
 }
